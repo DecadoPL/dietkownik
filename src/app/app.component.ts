92b993206ed944aab95291c0ab409554ce5b6b01
DELLCZYK\krzys <krzysztof.piontek@hotmail.com>
2024-02-07 19:49:24 +0100

    Removed interval function from frontend since it doesnt keep backend alive on free server. And created function in backend to keep it up.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
-import { PortionNameService } from './services/portionName.service';
-
-
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Dietkownik';
 
-  constructor(private portionNameService: PortionNameService){}
-  
-  ngOnInit(): void {
-    const interval$ = interval(60000);
-    
-    // Subscribe to the interval
-    interval$.subscribe(() => {
-      // Perform your background process here
-      this.portionNameService.getPortionNamesMONGO().subscribe((data) => {
-        let x = data;
-        console.log("xxx")
-      })
-    });
-  }
+export class AppComponent {
+  title = 'Dietkownik';
 }
